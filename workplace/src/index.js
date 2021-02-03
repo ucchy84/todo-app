b86dd57efeb672df04d0105e400dceee8d433b03
ucchy84 <k208576@ie.u-ryukyu.ac.jp>
2021-02-03 17:00:10 +0900

    refactoring

--- workplace/src/index.js
+++ workplace/src/index.js
@@ -3,26 +3,6 @@ import ReactDOM from 'react-dom';
 // import App from './App'
 
 class App extends React.Component {
-  
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     todos: [
-  //       {
-  //         id: 1,
-  //         title: "Hello, React!",
-  //         body: "React始めました!",
-  //         done: false
-  //       },
-  //       {
-  //         id: 2,
-  //         title: "Hello, Redux!",
-  //         body: "Reduxも始めました!",
-  //         done: false
-  //       },
-  //     ]
-  //   }
-  // }
 
   render() {
     const todos = [
