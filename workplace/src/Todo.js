f9b0697e7196761d955236ff425b477b95b2f891
ucchy84 <k208576@ie.u-ryukyu.ac.jp>
2021-02-15 13:37:22 +0900

    fix Funtion Component

--- workplace/src/Todo.js
+++ workplace/src/Todo.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
 
 class Todo extends React.Component {
 
   render() {
-    const className = 'unDone'
-    const link = this.props.done ? '元に戻す' : '完了'
+    // const className = 'unDone'
+    // const link = this.props.done ? '元に戻す' : '完了'
 
     return (
-      <li className={className}>
-        <span>{this.props.id}</span>
-        <span>{this.props.title}</span>
-        <a href="">{link}</a>
-        <p>{this.props.body}</p>
-      </li>
+      <TableRow key={this.props.id}>
+        <TableCell component="th" scope="row">
+          {this.props.id}
+        </TableCell>
+        <TableCell align="right">{this.props.title}</TableCell>
+        <TableCell align="right">{this.props.body}</TableCell>
+      </TableRow>
     )
 
   }
